f86cd410d7b015da187d76b34b471884607a0f48
Aniket Potdar <aniketpanditpotdar@gmail.com>
2023-11-10 15:26:37 +0530

    Add contact Api's

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -1,53 +1,13 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const Contact = require("../models/contactModel");
 const { body, validationResult } = require("express-validator");
+const ObjectId = require("mongoose").Types.ObjectId;
 const router = express.Router();
 
-/*=======================================================
-                        Login
-=========================================================*/
-
-router.post("/login", [body("email").not().isEmpty(), body("password").not().isEmpty()], async (req, res) => {
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({
-            error: errors.array()[0],
-        });
-    }
-
-    const emailExist = await User.findOne({ email: req.body.email });
-
-    if (emailExist) {
-        bcrypt.compare(req.body.password, emailExist.password, (error, result) => {
-            if (error) { return res.status(401).json(error) }
-            if (result == true) {
-                let token = jwt.sign({ email: emailExist.email }, "secret", {
-                    expiresIn: "90d",
-                });
-                emailExist.save().then(() => {
-                    return res.status(200).json({ status: 200, email: emailExist.email, token, role: emailExist.role })
-                }).catch((error) => {
-                    return res.status(404).json({ error, message: "Something went wrong" })
-                });
-            } else if (result == false) {
-                res.status(401).json({ error: "Invalid password" });
-            } else {
-                res.status(401).json({ error: "Please Enter valid credentials" });
-            }
-        })
-    } else {
-        res.status(401).json({ error: "Email Not Exist" });
-    }
-})
-
-/*=======================================================
-                     Create User Data
-=========================================================*/
-
-router.post("/register", [body("email").isEmail(), body("email").not().isEmpty(), body("password").not().isEmpty()],
+router.post("/createContact", [
+    body("firstName").not().isEmpty(),
+    body("lastName").not().isEmpty(),
+    body("phoneNo").not().isEmpty()],
     async (req, res) => {
 
         const errors = validationResult(req);
@@ -57,24 +17,83 @@ router.post("/register", [body("email").isEmail(), body("email").not().isEmpty()
             });
         }
 
-        const emailExist = await User.findOne({ email: req.body.email });
-
-        if (emailExist) return res.status(401).json({ error: "User with the E-mail already exists" });
-
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        const userDetails = {
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword,
+        const contactDetails = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            phoneNo: req.body.phoneNo,
         };
 
-        const newUser = new User(userDetails);
-        newUser.save().then((result) => {
-            return res.status(200).json({ msg: "User Registered Successfully!!!", details: result });
+        const newContact = new Contact(contactDetails);
+        newContact.save().then((result) => {
+            return res.status(200).json({ msg: "Contact Created Successfully!!!", details: result });
         }).catch((error) => {
             return res.status(401).json({ error: "Something Went Wrong!!!" });
         });
     }
 );
+
+/*=======================================================
+                    Get All Contact Data
+=========================================================*/
+
+router.get("/getContacts", async (req, res) => {
+    try {
+        const getContact = await Contact.find({});
+        res.send(getContact);
+    } catch (e) {
+        res.send(e);
+    }
+});
+
+/*=======================================================
+                    Get Contact by ID
+=========================================================*/
+
+router.get("/getContact/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+
+        if (!ObjectId.isValid(_id)) {
+            return res.status(400).send(`No records with given id : ${_id}`);
+        }
+
+        const getContact = await Contact.findById({ _id });
+        res.status(200).send(getContact);
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).send(e);
+    }
+});
+
+/*=======================================================
+                    Update Contact
+=========================================================*/
+
+router.put("/updateContact/:id", async (req, res) => {
+    try {
+        const _id = req.params.id;
+
+        const updateContact = await Contact.findByIdAndUpdate(_id, req.body, {
+            new: true
+        });
+        res.send(updateContact);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+/*=======================================================
+                    Delete Contact
+=========================================================*/
+
+router.delete("/deleteContact/:id", async (req, res) => {
+    try {
+        const deleteContact = await Contact.findByIdAndDelete(req.params.id)
+        res.send(deleteContact);
+    } catch (e) {
+        res.send(e);
+    }
+});
 
 module.exports = router;
