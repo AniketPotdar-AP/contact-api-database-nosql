1772789590dc41b9fa0d707cdd8992fe4f81dfbd
Aniket Potdar <aniketpanditpotdar@gmail.com>
2024-03-23 15:17:20 +0530

    Upload Files

--- server.js
+++ server.js
@@ -1,27 +1,28 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser")
-const app = express(); 
-const dotenv = require('dotenv')
+const cookieParser = require("cookie-parser");
+const app = express();
+const dotenv = require("dotenv");
 const port = process.env.PORT || 3000;
-const dbURI = "mongodb+srv://user:9CMLHeXvdwbd6sYd@contactcrud.ccdwvgc.mongodb.net/?retryWrites=true&w=majority";
+const dbURI =
+  "mongodb://user:zRpZV2HfzyEtDW6s@ac-wk0hijl-shard-00-00.mzwtfcv.mongodb.net:27017,ac-wk0hijl-shard-00-01.mzwtfcv.mongodb.net:27017,ac-wk0hijl-shard-00-02.mzwtfcv.mongodb.net:27017/?ssl=true&replicaSet=atlas-12xsaw-shard-0&authSource=admin&retryWrites=true&w=majority&appName=contactCrud";
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
-app.use(cookieParser())
+app.use(cookieParser());
 dotenv.config();
 
 mongoose
-    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(
-        () => console.log("Database Connected"),
-        app.listen(port, () => {
-            console.log(`Running on port ${port}`);
-        })
-    )
-    .catch((err) => console.log(err));
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(
+    () => console.log("Database Connected"),
+    app.listen(port, () => {
+      console.log(`Running on port ${port}`);
+    })
+  )
+  .catch((err) => console.log(err));
 
 const user = require("./api/routes/user");
 const contact = require("./api/routes/contact");
